3ffce9c9720c97560bc206ccde445d40a3b09e10
Keoabetswe <KeoNthite@gmail.com>
2020-08-21 23:30:32 +0200

    fix class syntax

--- js/list.js
+++ js/list.js
@@ -1,44 +1,46 @@
 const addTask = document.querySelector('.add');
 const addButton = document.querySelector('.add-todo');
 const todoList = document.querySelector('.todos');
-const list = document.querySelector('.todos li');
+const list = document.querySelectorAll('.todos li'); 
 
 let listLength = list.length;
 
-const taskTemplate = (todo) =>
+const taskTemplate = (todo) => 
 {
-    const html = `<li>
-                    <input type="checkbox" id="todo_${listLength}">
-                    <label for="todo_${listLength}">
-                        <span class="check"></span>
-                        ${todo}
-                    </label>
-                    <i class="far f-trash-alt delete"></i>
+  
+  const html = `<li>
+                  <input type="checkbox" id="todo_${listLength}">
+                  <label for="todo_${listLength}">
+                    <span class="check"></span>
+                    ${todo}
+                  </label>
+                  <i class="far fa-trash-alt delete"></i>
                 </li>`
-    todoList.innerHTML += html;
+  todoList.innerHTML += html;
 };
 
-function addTodos(e)
+function addTodos(e) 
 {
-    e.preventDefault();
-    const todo = addTask.nodeValue.trim();
-    if(todo.length)
-    {
-        listLength = listLength + 1;
-        taskTemplate(todo);
-        addTask.reset();
-    }
+  e.preventDefault();
+  const todo = addTask.add.value.trim();
+  
+  if (todo.length)
+   {
+    listLength = listLength + 1;
+    taskTemplate(todo);
+    addTask.reset();
+  }
 }
 
-addTask.addEventListener('submit', addTodos),
+addTask.addEventListener('submit', addTodos);
 addButton.addEventListener('click', addTodos);
 
-function deleteTodos(e)
+function deleteTodos(e) 
 {
-    if(e.target.classList.contains('delete'))
-    {
-        e.target.parentElement.remove();
-    }
+  if (e.target.classList.contains('delete')) 
+  {
+    e.target.parentElement.remove();
+  }
 }
 
 todoList.addEventListener('click', deleteTodos);
